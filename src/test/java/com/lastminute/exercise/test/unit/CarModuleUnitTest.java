c413b83ca62731af834e225763c8f6a9055a0e66
Gianluca Colaianni <fast_luca@hotmail.it>
2018-06-15 08:41:56 +0200

    Added CarModule unit test

---
+++ src/test/java/com/lastminute/exercise/test/unit/CarModuleUnitTest.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.lastminute.exercise.test.unit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.lastminute.exercise.injection.CartModule;
+import com.lastminute.exercise.service.CartItemService;
+import com.lastminute.exercise.service.CartItemServiceImpl;
+import com.lastminute.exercise.service.CartService;
+import com.lastminute.exercise.service.CartServiceImpl;
+import com.lastminute.exercise.service.TaxCalculator;
+import com.lastminute.exercise.service.TaxCalculatorImpl;
+
+/**
+ * @author Gianluca Colaianni -- Fincons Group S.p.A.
+ *
+ */
+public class CarModuleUnitTest {
+    
+    private static Injector injector;
+
+    @BeforeClass
+    public static void setUp() {
+	injector = Guice.createInjector(new CartModule());
+    }
+
+    @Test
+    public void test() {
+	assertThat(injector.getInstance(CartService.class)).isInstanceOf(CartServiceImpl.class);
+	assertThat(injector.getInstance(CartItemService.class)).isInstanceOf(CartItemServiceImpl.class);
+	assertThat(injector.getInstance(TaxCalculator.class)).isInstanceOf(TaxCalculatorImpl.class);
+    }
+
+}
